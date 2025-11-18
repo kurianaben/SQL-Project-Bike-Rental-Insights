d2723eff10c818300d76479f8f9ee0e52529e906
kurianaben <bkurian36@gmail.com>
2025-11-18 13:02:57 -0600

    Update 2_change_over_time.sql

--- SQL_Queries/2_change_over_time.sql
+++ SQL_Queries/2_change_over_time.sql
@@ -16,3 +16,36 @@ SELECT
 FROM bike_data
 GROUP BY date
 ORDER BY date;
+
+-- 2. Monthly total rentals
+SELECT 
+    date_trunc('month', date) AS month,
+    SUM(bike_rental_count)    AS monthly_rentals
+FROM bike_data
+GROUP BY month
+ORDER BY month;
+
+-- 3. Average rentals by hour of day
+SELECT 
+    hour,
+    AVG(bike_rental_count) AS avg_rentals
+FROM bike_data
+GROUP BY hour
+ORDER BY hour;
+
+-- 4. Averages on Weekday vs weekend
+SELECT
+    CASE WHEN EXTRACT(DOW FROM date) IN (0,6) THEN 'Weekend' ELSE 'Weekday' END AS day_type,
+    AVG(bike_rental_count) AS avg_rentals,
+    SUM(bike_rental_count) AS total_rentals
+FROM bike_data
+GROUP BY day_type;
+
+-- 5. Average Hourly pattern by season
+SELECT 
+    seasons,
+    hour,
+    AVG(bike_rental_count) AS avg_rentals
+FROM bike_data
+GROUP BY seasons, hour
+ORDER BY seasons, hour;
